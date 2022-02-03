bd5a2cbcfb45598dda1fa9bfc9afc36a14d84e35
Miguel Taylor <migueltaylor.tx@gmail.com>
2022-02-03 12:04:31 -0600

    variables

--- apps.js
+++ apps.js
@@ -1,2 +1,7 @@
-var message = 'Welcome';
-console.log(message);
+var message = 'Welcome'; //string variable 
+console.log(message); // console variable
+let a, b, c;
+a = 3;
+b = 6;
+c = a + b;
+document.getElementById("math").innerHTML = c;
