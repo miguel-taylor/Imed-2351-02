485e9987ab5892e30d2d555a2b278e9fe6c90057
Miguel Taylor <migueltaylor.tx@gmail.com>
2022-02-03 12:39:37 -0600

    finish

--- apps.js
+++ apps.js
@@ -1,7 +1,12 @@
 var message = 'Welcome'; //string variable 
 console.log(message); // console variable
-let a, b, c;
+let a, b, c; // Global variable
 a = 3;
 b = 6;
 c = a + b;
 document.getElementById("math").innerHTML = c;
+{ 
+    let x =12; //block variable
+}
+const food = "banana";
+document.getElementById("sentence").innerHTML = "A monkey's favorite food is a ..." + food;
